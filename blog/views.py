bb951621a9af4ed44e1453fb6e9912e4e28d8bbb
Fiona-T <fitracey@gmail.com>
2021-10-19 11:09:08 +0000

    add view, template and url pattern for home page index.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+# for generic views - part of class based views
+from django.views import generic
+from .models import Post
 
 # Create your views here.
+
+class PostList(generic.ListView):
+    # use the Post class as the model
+    model = Post
+    # get the data from table, filtered by published status, descending order
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
